a2ff0865d07ed542358bcb44e7d88698dfccdd5b
DevDaveF <David.Filepp@gmail.com>
2024-05-15 16:20:37 -0500

    Completed exerise 1.5

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -1,46 +1,50 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header name={course} /> 
-      <Content parts = {parts}/> 
-      <Total parts = {parts} />
+      <Header course={course} /> 
+      <Content parts={course}/> 
+      <Total parts={course} />
     </div>
   )
 }
 
-const Header = (course) => {
+const Header = (props) => {
   return(
     <div>
-      <h1>{course.name}</h1>
+      <h1>{props.course.name}</h1>
     </div>
   )
 }
 
 const Content = (stuff) => {
+  console.log({stuff})
   return (
     <div>
-      < Part1 name = {stuff.parts[0].name} numbers = {stuff.parts[0].exercises} />
-      < Part2 name = {stuff.parts[1].name} numbers = {stuff.parts[1].exercises} />
-      < Part3 name = {stuff.parts[2].name} numbers = {stuff.parts[2].exercises} />
+      < Part1 name = {stuff.parts.parts[0].name} numbers = {stuff.parts.parts[0].exercises} />
+      < Part2 name = {stuff.parts.parts[1].name} numbers = {stuff.parts.parts[1].exercises} />
+      < Part3 name = {stuff.parts.parts[2].name} numbers = {stuff.parts.parts[2].exercises} />
     </div>
   )
 } 
+
 
 const Part1 = (parts1) => {
   return(
@@ -63,7 +67,7 @@ const Part3 = (parts3) => {
 const Total = (totals) =>{
   return(
     <div>
-      <p>Number of exercises {totals.parts[0]['exercises'] + totals.parts[1]['exercises'] + totals.parts[2]['exercises']}</p>
+      <p>Number of exercises {totals.parts.parts[0]['exercises'] + totals.parts.parts[1]['exercises'] + totals.parts.parts[2]['exercises']}</p>
     </div>
   )
 }
