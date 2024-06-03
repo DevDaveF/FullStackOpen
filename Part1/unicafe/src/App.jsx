3af1db6a5537381aad48dbc56bd3834d47e0fb6c
DevDaveF <David.Filepp@gmail.com>
2024-06-03 10:26:37 -0500

    Completed exercise 1.10

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -7,15 +7,24 @@ const Button = (props) => (
 )
 
 const Statistics =(stats) => {
+  const total = stats.goodStat+stats.neutralStat+stats.badStat
+  const averageScore = (stats.goodStat - stats.badStat)/total 
+  const percentPositive = (stats.goodStat / total) * 100 + " %"
   return (
     <div>
-    <p>Good = {stats.goodStat} </p>
-    <p>Neutral = {stats.neutralStat}</p>
-    <p>Bad = {stats.badStat}</p>
-    <p>Total = {stats.totalStat}</p>
-    <p>Average Score = {stats.avgStat}</p>
-    <p>Percentage Positive = {stats.pctStat}</p>
+    <StatisticLine text="Good = " value={stats.goodStat}/>
+    <StatisticLine text="Neutral = " value={stats.neutralStat}/>
+    <StatisticLine text="Bad = " value={stats.badStat}/>
+    <StatisticLine text="Total = " value={total}/>
+    <StatisticLine text="Average Score = " value={averageScore}/>
+    <StatisticLine text="Percentage Positive = " value={percentPositive}/>
     </div>
+  )
+}
+
+const StatisticLine =(line) => {
+  return (
+    <p>{line.text} {line.value}</p>
   )
 }
 
@@ -23,11 +32,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const total = good+neutral+bad
-  const averageScore = (good - bad)/total 
-  const percentPositive = (good / total) * 100
+  const feedback = good+neutral+bad
 
-  if (total >0){
+  if (feedback >0){
     return (
       <div>
         <h1>Give Feedback</h1>
@@ -35,7 +42,7 @@ const App = () => {
         <Button handleClick={() => setNeutral(neutral +1)} text="Neutral" />
         <Button handleClick={() => setBad(bad +1)} text="Bad" />
         <h2>Statistics</h2>
-        <Statistics goodStat = {good} neutralStat = {neutral} badStat = {bad} totalStat = {total} avgStat={averageScore} pctStat={percentPositive}/>
+        <Statistics goodStat = {good} neutralStat = {neutral} badStat = {bad}/>
       </div>
     )
   }
